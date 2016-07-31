1e6fb61d647e265aef80b84a207acf2fcc6c2109
Ryan Wang <rywang@outlook.com>
2016-07-30 21:34:10 -0700

    Add logging

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,9 @@ import Control.Monad (void)
 import System.IO (readFile)
 import qualified Data.Text as Text
 
+import Data.Time.Format
+import Data.Time.LocalTime
+
 import Network.Linklater
 import Network.Wai.Handler.Warp (run)
 
@@ -23,11 +26,26 @@ channel = GroupChannel $ text "general"
 
 -- End bot configuration
 
+-- Begin logging utils
+
+defaultFormat :: String
+defaultFormat = "%F %T %Z"
+
+-- TODO: Get a real logging framework
+logLine :: String -> IO ()
+logLine s = do
+  zonedTime <- getZonedTime
+  let timestamp = formatTime defaultTimeLocale defaultFormat zonedTime
+  putStrLn ("[" ++ timestamp ++ "] " ++ s)
+
+-- End logging utils
+
 formatMessage :: Text.Text -> Message
 formatMessage text = SimpleMessage icon name channel text
 
 stravaBot :: Config -> Maybe Command -> IO Text.Text
 stravaBot config (Just command) = do
+  logLine ("Received a command: " ++ show command)
   void $ say (formatMessage $ _commandName command) config
   return $ text "Handled a command!\n"
 stravaBot config Nothing = return $ text "Failed to parse an incoming command...\n"
@@ -35,6 +53,6 @@ stravaBot config Nothing = return $ text "Failed to parse an incoming command...
 main :: IO ()
 main = do
   slackUrl <- filter (/= '\n') <$> readFile "secrets/slack-webhook-url"
-  putStrLn ("+ Listening on port " ++ show port)
+  logLine ("Listening on port " ++ show port)
   run port (slashSimple (stravaBot $ Config $ text slackUrl))
   where port = 8000
